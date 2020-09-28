64d6f4039918fe53146897365c476c97528e306a
BK <benkoshy@hotmail.com>
2020-09-29 09:51:37 +1000

    WIP: add vector exercises

---
+++ secret_life_of_objects/vector.js
@@ -0,0 +1,47 @@
+class Vector{
+  constructor(x,y){
+    this._x = x
+    this._y = y
+  }
+
+  get x(){
+    return this._x
+  }
+
+  set x(value){
+    this._x = value;
+  }
+
+  get y(){
+    return this._y
+  }
+
+  set y(value){
+    this._y = value;
+  }
+
+  plus(vector){
+    return new Vector(vector.x + this.x, vector.y + this.y)
+  }
+
+  minus(vector){
+    return new Vector(this.x - vector.x, this.y - vector.y)
+  }
+}
+
+Vector.prototype["length"] = function(){  
+  return Math.sqrt(( Math.pow(this.x,2) + (Math.pow(this.y, 2))));
+}
+
+
+/*
+Vector.prototype["length"] = {  
+  get length() {Math.sqrt(( Math.pow(this.x,2) + (Math.pow(this.y, 2))));}
+}
+
+*/
+
+
+
+
+module.exports = Vector;
---
+++ secret_life_of_objects/vector.test.js
@@ -0,0 +1,27 @@
+
+const Vector = require('./vector');
+
+test('vector plus method', () => {
+  v = new Vector(1,2);   
+
+  expect(v.plus(v) instanceof Vector).toBe(true)
+  expect(v.plus(v).x).toBe(2)
+  expect(v.plus(v).y).toBe(4)
+});
+
+test('vector minus method', () => {
+  v = new Vector(1,2);   
+
+  expect(v.minus(v) instanceof Vector).toBe(true)
+  expect(v.minus(v).x).toBe(0)
+  expect(v.minus(v).y).toBe(0)
+});
+
+
+test('length property added as a prototype', () => {
+  v = new Vector(3,4);   
+  expect(v.length()).toBe(5)  
+});
+
+
+
