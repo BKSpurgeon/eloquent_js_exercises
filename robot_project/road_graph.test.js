483fcb5a82db579daf7010734d215da4f06dff63
koshy <ben.chenathara@gmail.com>
2020-10-03 11:53:52 +1000

    WIP: robots - add village class

--- robot_project/road_graph.test.js
+++ robot_project/road_graph.test.js
@@ -1,7 +1,16 @@
 const roadGraph = require('./road_graph');
 
+const Village = require('./village');
+
 test("gets all destinations to alice's house", () => {
   alices_destinations = ["Bob's House", "Cabin", "Post Office"]
 
   expect(roadGraph["Alice's House"]).toEqual(alices_destinations)
+})
+
+test("Village class", () => {
+  parcels = [{place: "Post Office", address: "Alice's House"}];
+  old_village = new Village("Alice's House", parcels);
+
+  expect(old_village.move("Alice's House")).toEqual(old_village)
 })
